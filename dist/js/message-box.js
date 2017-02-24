68eb6aa7dac240f4b6340328b5f222bf9fa9fbde
Francisco Vallarino <fjvallarino@gmail.com>
2017-02-24 19:38:58 -0300

    Add message box service utility

--- dist/js/message-box.js
+++ dist/js/message-box.js
@@ -1,6 +1,34 @@
 "use strict";
 
-angular.module("risevision.common.components.message-box", [])
+angular.module("risevision.common.components.message-box.services", [])
+  .factory("messageBox", ["$q", "$log", "$modal", "$templateCache",
+    function ($q, $log, $modal, $templateCache) {
+      return function (title, message, close) {
+        var modalInstance = $modal.open({
+          template: $templateCache.get("message-box/message-box.html"),
+          controller: "messageBoxInstance",
+          windowClass: "modal-custom",
+          resolve: {
+            title: function () {
+              return title;
+            },
+            message: function () {
+              return message;
+            },
+            button: function () {
+              return close || "common.close";
+            }
+          }
+        });
+      };
+    }
+  ]);
+
+"use strict";
+
+angular.module("risevision.common.components.message-box", [
+  "risevision.common.components.message-box.services"
+])
   .controller("messageBoxInstance", ["$scope", "$modalInstance",
     "title", "message", "button",
     function ($scope, $modalInstance, title, message, button) {
