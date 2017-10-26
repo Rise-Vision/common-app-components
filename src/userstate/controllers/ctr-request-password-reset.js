9f0ce2ca6f722682df4a4d974d7b2ad0677fc73b
Francisco Vallarino <fjvallarino@gmail.com>
2017-10-26 19:24:30 -0300

    Do not allow requesting password reset for Google accounts

--- src/userstate/controllers/ctr-request-password-reset.js
+++ src/userstate/controllers/ctr-request-password-reset.js
@@ -8,21 +8,32 @@ angular.module("risevision.common.components.userstate")
       $scope.credentials = {};
       $scope.errors = {};
       $scope.emailSent = false;
+      $scope.isGoogleAccount = false;
 
       $scope.requestPasswordReset = function () {
         $scope.emailSent = false;
+        $scope.isGoogleAccount = false;
         $loading.startGlobal("auth-request-password-reset");
 
         userauth.requestPasswordReset($scope.credentials.username)
           .then(function () {
             $log.log("Reset password request sent");
+            $scope.emailSent = true;
           })
           .catch(function (err) {
-            $log.error(err);
+            var error = err && err.result && err.result.error && err.result
+              .error.message;
+
+            if (error === "Google account") {
+              $log.log("Requested password reset for Google account");
+              $scope.isGoogleAccount = true;
+            } else {
+              $log.error(err);
+              $scope.emailSent = true;
+            }
           })
           .finally(function () {
             $loading.stopGlobal("auth-request-password-reset");
-            $scope.emailSent = true;
           });
       };
     }
